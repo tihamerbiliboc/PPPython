9da05d024107acc824cbb11d083bfe86a4a90574
Tihamer Biliboc <tiha0006@easv365.dk>
2022-03-18 10:20:18 +0100

    minimal gui executing sequential prime generator

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import prime_generator
 import timeit
+import gui_prime
 
 
 
 if __name__ == '__main__':
-    start = timeit.default_timer()
-    print(prime_generator.generate_primes(10, 1000000))
-    stop = timeit.default_timer()
-    execution_time = stop - start
-    print("Program Executed in " + str(execution_time) + "  seconds")
+    # start = timeit.default_timer()
+    # print(prime_generator.generate_primes(10, 1000000))
+    # stop = timeit.default_timer()
+    # execution_time = stop - start
+    # print("Program Executed in " + str(execution_time) + "  seconds")
+    gui_prime.window.mainloop()
 
 
 
